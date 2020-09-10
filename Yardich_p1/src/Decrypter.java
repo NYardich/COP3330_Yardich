a54ce888240ac38a34afc15f4eb444de0a231482
NYardich <nickyardich@gmail.com>
2020-09-10 19:36:48 -0400

    Revised variable names and removed redundant comments

--- Yardich_p1/src/Decrypter.java
+++ Yardich_p1/src/Decrypter.java
@@ -1,35 +1,30 @@
 public class Decrypter {
 
-    public static int[] strConvert(String num) {
-        int[] res = new int[4];
-
-        // Iterate through res
-        for(int i = 0; i < 4; i++) {
-            // Assign character i in num to digit i in res
-            res[i] = Character.getNumericValue(num.charAt(i));
-        }
-        return res;
-    }
-
     public String decrypt(String num) {
-        int[] res = strConvert(num);
+        int[] numIntArr = convertStringToIntArr(num);
         char[] decrypted = new char[4];
 
-        // Decrypt each digit
         for(int i = 0; i < 4; i++) {
-            // Add to character array in correct index
-            decrypted[(i + 2) % 4] = numberUnmix(res[i]);
+            // Add decrypted char in correct index
+            decrypted[(i + 2) % 4] = numberUnmix(numIntArr[i]);
         }
 
-        // Return string version of character array
         return String.valueOf(decrypted);
+    }
+
+    public static int[] convertStringToIntArr(String num) {
+        int[] numIntArr = new int[4];
+
+        for(int i = 0; i < 4; i++) {
+            numIntArr[i] = Character.getNumericValue(num.charAt(i));
+        }
+        return numIntArr;
     }
 
     public static char numberUnmix(int currNum) {
         // Adding 3 and modding by 10 reverses the encryption on the number
         currNum = (currNum + 3) % 10;
 
-        // Return number as char
         return (char) (currNum + '0');
     }
 }
--- Yardich_p1/src/Encrypter.java
+++ Yardich_p1/src/Encrypter.java
@@ -1,34 +1,30 @@
 public class Encrypter {
 
-    public static int[] strConvert(String num) {
-        int[] res = new int[4];
-
-        // Iterate through res
-        for(int i = 0; i < 4; i++) {
-            // Assign character i in num to digit i in res
-            res[i] = Character.getNumericValue(num.charAt(i));
-        }
-        return res;
-    }
-
-    public static String encrypt(String num) {
-        int[] res = strConvert(num);
+    public String encrypt(String num) {
+        int[] numIntArr = convertStringToIntArr(num);
         char[] encrypted = new char[4];
 
-        // Encrypt each digit
         for(int i = 0; i < 4; i++) {
-            // Add to character array in correct index
-            encrypted[(i + 2) % 4] = numberMix(res[i]);
+            // Add encrypted char in correct index
+            encrypted[(i + 2) % 4] = numberMix(numIntArr[i]);
         }
-        // Return string version of character array
+
         return String.valueOf(encrypted);
     }
 
+    public static int[] convertStringToIntArr(String num) {
+        int[] numIntArr = new int[4];
+
+        for(int i = 0; i < 4; i++) {
+            numIntArr[i] = Character.getNumericValue(num.charAt(i));
+        }
+        return numIntArr;
+    }
+
     public static char numberMix(int currNum) {
-        // Perform operations necessary
+        // Perform encryption operations
         currNum = (currNum + 7) % 10;
 
-        // Return number as char
         return (char) (currNum + '0');
     }
 }
