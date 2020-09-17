85863b1b730eb232392a44d9e3daf42cb7206731
NYardich <nickyardich@gmail.com>
2020-09-16 22:54:48 -0400

    First Solution with required methods. No tests

--- Yardich_p2/src/App.java
+++ Yardich_p2/src/App.java
@@ -1,2 +1,86 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class App {
+
+    public static void main(String[] args) {
+        ArrayList<BodyMassIndex> bmiData = new ArrayList<BodyMassIndex>();
+
+        while (moreInput()) {
+            double height = getUserHeight();
+            double weight = getUserWeight();
+
+            BodyMassIndex bmi = new BodyMassIndex(height, weight);
+            bmiData.add(bmi);
+
+            displayBmiInfo(bmi);
+        }
+
+        displayBmiStatistics(bmiData);
+    }
+
+    private static boolean moreInput() {
+        Scanner input = new Scanner(System.in);
+        String response;
+
+        System.out.printf("Enter Y to continue or N to stop:\n\t> ");
+        response = input.next();
+        switch(response) {
+            case "Y":
+                return true;
+            case "N":
+                return false;
+            default:
+                System.out.println("Answer should be Y or N");
+        }
+        return false;
+    }
+
+    public static double getUserHeight() {
+        Scanner input = new Scanner(System.in);
+        double height = 0;
+        while(true) {
+            System.out.printf("Enter Height in Inches:\n\t> ");
+            height = input.nextDouble();
+            input.nextLine();
+            if (height > 0) {
+                return height;
+            } else {
+                System.out.println("Height must be positive.");
+            }
+        }
+    }
+
+    public static double getUserWeight() {
+        Scanner input = new Scanner(System.in);
+        double weight = 0;
+        while(true) {
+            System.out.printf("Enter Weight in Pounds:\n\t> ");
+            weight = input.nextDouble();
+            input.nextLine();
+            if (weight > 0) {
+                return weight;
+            } else {
+                System.out.println("Weight must be positive.");
+            }
+        }
+    }
+
+    public static void displayBmiInfo(BodyMassIndex bmi) {
+        System.out.printf("BMI: %.2f\n", bmi.BMIScore());
+        bmi.BMICategory();
+    }
+
+    public static void displayBmiStatistics(ArrayList<BodyMassIndex> bmi) {
+        double total = getAverageBMI(bmi);
+        System.out.printf("Average of all BMIs: %.2f\n", total);
+    }
+
+    public static double getAverageBMI(ArrayList<BodyMassIndex> bmi) {
+        double total = 0;
+        for (BodyMassIndex user : bmi) {
+            total += user.BMIScore();
+        }
+        return total / bmi.size();
+    }
 }
--- Yardich_p2/src/BodyMassIndex.java
+++ Yardich_p2/src/BodyMassIndex.java
@@ -1,2 +1,30 @@
+import java.util.Scanner;
 public class BodyMassIndex {
+    // Attributes
+    private double height;
+    private double weight;
+
+    // Constructor
+    public BodyMassIndex(double height, double weight) {
+        this.height = height;
+        this.weight = weight;
+    }
+
+    // Methods
+    public double BMIScore() {
+        return (703 * this.weight) / Math.pow(this.height, 2);
+    }
+
+    public void BMICategory() {
+        double score = this.BMIScore();
+        if (score <= 18.5) {
+            System.out.println("Underweight");
+        } else if (score <=24.9) {
+            System.out.println("Normal weight");
+        } else if (score <= 29.9) {
+            System.out.println("Overweight");
+        } else {
+            System.out.println("Obesity");
+        }
+    }
 }
