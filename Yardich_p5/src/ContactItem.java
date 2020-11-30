3b63e0fb7fb32b7a9b8e0c5d58a31b3dfb43a2c6
NYardich <nickyardich@gmail.com>
2020-11-29 20:14:03 -0500

    decomposes empty-check in ContactItem into its own method

--- Yardich_p5/src/ContactItem.java
+++ Yardich_p5/src/ContactItem.java
@@ -7,7 +7,7 @@ public class ContactItem {
     // Constructor
     ContactItem(String firstName, String lastName, String phoneNumber, String emailAddress) throws Exception {
         // Checks if all fields are blank
-        if (firstName.isBlank() && lastName.isBlank() && phoneNumber.isBlank() && emailAddress.isBlank()) {
+        if (!hasSomething(firstName, lastName, phoneNumber, emailAddress)) {
             throw new Exception("There must be at least one parameter in the Contact.");
         }
         this.firstName = firstName;
@@ -16,63 +16,41 @@ public class ContactItem {
         this.emailAddress = emailAddress;
     }
 
-    // Setters and Getters
-    // Each setter must ensure that changing its value will not make the entire item empty
+    // Getters
 
     public String getFirstName() {
         return firstName;
-    }
-
-    public void setFirstName(String firstName) throws Exception {
-        if (firstName.isBlank() && lastName.isBlank() && phoneNumber.isBlank() && emailAddress.isBlank()) {
-            throw new Exception("There must be at least one parameter in the Contact.");
-        }
-        this.firstName = firstName;
     }
 
     public String getLastName() {
         return lastName;
     }
 
-    public void setLastName(String lastName) throws Exception {
-        if (firstName.isBlank() && lastName.isBlank() && phoneNumber.isBlank() && emailAddress.isBlank()) {
-            throw new Exception("There must be at least one parameter in the Contact.");
-        }
-        this.lastName = lastName;
-    }
-
     public String getPhoneNumber() {
         return phoneNumber;
-    }
-
-    public void setPhoneNumber(String phoneNumber) throws Exception {
-        if (firstName.isBlank() && lastName.isBlank() && phoneNumber.isBlank() && emailAddress.isBlank()) {
-            throw new Exception("There must be at least one parameter in the Contact.");
-        }
-        this.phoneNumber = phoneNumber;
     }
 
     public String getEmailAddress() {
         return emailAddress;
     }
 
-    public void setEmailAddress(String emailAddress) throws Exception {
-        if (firstName.isBlank() && lastName.isBlank() && phoneNumber.isBlank() && emailAddress.isBlank()) {
-            throw new Exception("There must be at least one parameter in the Contact.");
-        }
-        this.emailAddress = emailAddress;
-    }
-
-
     // General editItem for changing all values at once
     public void editItem(String firstName, String lastName, String phoneNumber, String emailAddress) throws Exception {
-        if (firstName.isBlank() && lastName.isBlank() && phoneNumber.isBlank() && emailAddress.isBlank()) {
+        if (!hasSomething(firstName, lastName, phoneNumber, emailAddress)) {
             throw new Exception("There must be at least one parameter in the Contact.");
         }
         this.firstName = firstName;
         this.lastName = lastName;
         this.phoneNumber = phoneNumber;
         this.emailAddress = emailAddress;
+    }
+
+    // Checks if the given parameters contain at least one non-empty item
+    public boolean hasSomething(String firstName, String lastName, String phoneNumber, String emailAddress) {
+        if (firstName.isBlank() && lastName.isBlank() && phoneNumber.isBlank() && emailAddress.isBlank()) {
+            return false;
+        }
+        return true;
     }
 
     // Formatted String for single Contact Item
